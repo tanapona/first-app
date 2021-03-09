b326a609e6145da806de5db6c9fe2fdbcc1b6118
tanapon <tanapon.auta@gmail.com>
2021-03-09 16:46:02 +0700

    second app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My First App'),
+        title: Text('My Second App'),
       ),
     );
   }
